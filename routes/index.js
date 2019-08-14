4c68ac0f0a93922b4d9df4fc3466f4ab2fed7963
aramys <aramys.antoniazzi@gmail.com>
2019-08-13 21:57:14 -0300

    Adicionado bootstrap e configurado pagina inicial exibindo posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var postService = require('../services/postsService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var posts = postService.getPosts();
+
+  res.render('index', { title: 'Blog', posts: posts });
 });
 
 module.exports = router;
