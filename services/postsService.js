17356205a2264d0da9dca2b502ccc5b1aebf16db
aramys <aramys.antoniazzi@gmail.com>
2019-08-20 20:28:20 -0300

    Adicionado funcionalidade de vizualização de posts

--- services/postsService.js
+++ services/postsService.js
@@ -1,12 +1,14 @@
 var getPosts = function(){
     var posts = [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
             description: 'Meu primeiro post',
             body: 'teste111111'   
         },
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
             description: 'Meu segundo post',
