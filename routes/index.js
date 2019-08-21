b9977b29d2dad3f8caed76716218458e06f4cf54
aramys <aramys.antoniazzi@gmail.com>
2019-08-20 21:33:28 -0300

    Criando menu e telas view /home e /all

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var postService = require('../services/postsService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var posts = postService.getPosts();
+  var posts = postService.getPosts().slice(0, 3);
 
   res.render('index', { title: 'Blog', posts: posts });
 });
@@ -16,6 +16,11 @@ router.get('/post/:postId' ,function(req, res, next){
   var post = posts.filter((post) => post.id == postId)[0];
 
   res.render('post', { title: post.title, post: post});
+});
+
+router.get('/all', function(req, res, next) {
+  var posts = postService.getPosts();
+  res.render('index', { title: 'Blog', posts: posts });
 });
 
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -14,6 +14,27 @@ var getPosts = function(){
             description: 'Meu segundo post',
             body: 'teste22222'  
         },
+        {
+            id: 3,
+            title: 'Post 3',
+            image: 'post3.jpg',
+            description: 'Meu terceito post',
+            body: 'teste33333333'  
+        },
+        {
+            id: 4,
+            title: 'Post 4',
+            image: 'post4.jpg',
+            description: 'Meu quarto post',
+            body: 'teste44444'  
+        },
+        {
+            id: 5,
+            title: 'Post 5',
+            image: 'post5.jpg',
+            description: 'Meu quinto post',
+            body: 'teste555555'  
+        }
     ];
     return posts;
 }
