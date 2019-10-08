e713983064d47ea8e157e6e03359f4879adf8cb4
aramys <aramys.antoniazzi@gmail.com>
2019-10-08 18:53:46 -0300

    Implementando funções de inclusão de objeto

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -16,4 +16,20 @@ router.get('/create', function(req, res, next) {
     res.render('admin/posts/create');
 });
 
+router.post('/create', function(req, res, next) {
+    var posts = postService.getPosts();
+
+    var newPost = {};
+
+    newPost.id = posts.length + 1;
+    newPost.title = req.body.title;
+    newPost.description = req.body.description;
+    newPost.image = "post1.jpg";
+    newPost.body = req.body.postBody;
+
+    postService.savePost(newPost);
+
+    res.redirect('/admin/posts');
+});
+
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -9,7 +9,25 @@ var getPosts = function(){
     return posts;
 }
 
+var saveFilePosts = function(posts){
+    var data = JSON.stringify(posts);
+    fs.writeFileSync(postsFilePath, data, 'utf8');
+}
+
+
+var loadFilePosts = function() {
+    var fileData = fs.readFileSync(postsFilePath, 'utf8');
+    var posts = JSON.parse(fileData);
+    return posts;
+}
+
+var savePost = function(newPost) {
+    var posts = loadFilePosts();
+    posts.push(newPost);
+    saveFilePosts(posts);
+}
 
 module.exports = {
-    getPosts: getPosts
+    getPosts: getPosts,
+    savePost: savePost
 }
